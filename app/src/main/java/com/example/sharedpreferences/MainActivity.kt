c32d417a390ce22838798338ebce9d530a235ea6
Poornajith <chiranthapoornajith@gmail.com>
2022-03-28 19:09:47 +0530

    version-1

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val sharedPreferences = getSharedPreferences("SP_INFO", MODE_PRIVATE)
+
+
+        btnSave.setOnClickListener{
+
+            val nameIn = txtNameIn.text.toString().trim()
+            val stats = switchYes.isChecked
+
+            val editor=sharedPreferences.edit()
+
+            editor.putString("String",nameIn)
+            editor.putBoolean("Boolean_value",stats)
+            editor.apply()
+        }
+
+        btnShowData.setOnClickListener{
+
+            val nameOut = sharedPreferences.getString("String","")
+            val switchYes = sharedPreferences.getBoolean("Boolean_value",false)
+
+            txtDataOut.text="String : $nameOut \nBoolean value : $switchYes"
+
+        }
+
     }
 }
